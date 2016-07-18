1b4fb7ee8f7920657c20b132062d3d33bd039094
doctornkz <doctornkz@yandex.ru>
2016-07-18 03:12:42 +0300

    Second screen removed. Social Share menu item added. Icon face lifting passed.

--- app/src/main/java/com/local/doctor/jkh_lighter/MainActivity.java
+++ app/src/main/java/com/local/doctor/jkh_lighter/MainActivity.java
@@ -1,5 +1,8 @@
 package com.local.doctor.jkh_lighter;
 
+//TODO: Add Local Storage for Temporary Measurements.
+//TODO: Add Settings for Sections - Energy, Water, Gas, Custom Fields.
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,10 +10,10 @@ import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.*;
 import android.util.Log;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                     camera.setParameters(p);
                     camera.startPreview();
                     Log.d(TAG, "Camera switched ON by switch");
-                }else{
+                } else {
                     Log.d(TAG, "Else statement!");
                     if (camera == null) {
                         Log.d(TAG, "Check = false, Camera service gone away...");
@@ -81,44 +84,33 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "Outbound from switch...");
 
-        Button btnOk;
-        btnOk = (Button) findViewById(R.id.btnOk);
+    }
 
-        View.OnClickListener oclBtnOk = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "Button OK Pressed. Ready to kill camera.");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        Log.d(TAG, "Menu initialize");
+        return true;
+    }
 
-                if (camera != null) {
-                    final Parameters p = camera.getParameters();
-                    Log.d(TAG, "Press OK, Camera is not null");
-                    p.setFlashMode(Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    camera.stopPreview();
-                    camera.release();
-                    camera = null;
-                } else {
-                    Log.d(TAG, "Button OK Pressed, Camera is null");
-                }
-
-                Log.d(TAG, "Camera switched OFF in Activity");
-
-//TODO: Add string memory in two direction
-
-                Intent intent = new Intent(v.getContext(), Main2Activity.class);
-                Log.d(TAG, "Intent initialize...");
-
-                intent.putExtra("t1", t1.getText().toString());
-                intent.putExtra("t2", t2.getText().toString());
-                intent.putExtra("t3", t3.getText().toString());
-                intent.putExtra("cold", cold.getText().toString());
-                intent.putExtra("hot", hot.getText().toString());
-
-                Log.d(TAG, "Try to change Activity...");
-                startActivity(intent);
-            }
-        };
-        btnOk.setOnClickListener(oclBtnOk);
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final String energy = "Energy: T1:" + t1.getText().toString() + " T2:" + t2.getText().toString() + " T3:" + t3.getText().toString();
+        final String water = "Water: Cold:" + cold.getText().toString() + " Hot:" + hot.getText().toString();
+        String message = energy + "\n" + water;
+        int id = item.getItemId();
+        Log.d(TAG, "Menu item Share pressed.");
+        switch (id) {
+            case R.id.share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Utilities: " + "\n" + message);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                Log.d(TAG, "Sending utilities info...");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
